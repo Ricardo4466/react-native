558fa1ea7fbea767e21e11a910fbc2a68f490012
Ricardo Teixeira Lima <rteixeira@mkom.tec.br>
2022-06-17 14:16:34 -0300

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={require('./assets/MKOM_Simbolo_Pos_RGB_Verm.png')}
+        style={{
+          marginBottom: 20
+        }}
+      />
+      <Text style={{
+        fontSize: 50,
+        color: '#F53340'
+      }}>
+        Hello
+        <Text>
+          {' '}
+        </Text>
+        <Text style={{
+          fontSize: 50,
+          color: 'rgb(0, 23, 138)'
+        }}>
+          World :)
+        </Text>
+      </Text>
+
       <StatusBar style="auto" />
     </View>
   );
